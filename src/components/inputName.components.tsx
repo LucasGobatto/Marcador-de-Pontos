59e7d4cf54991cdb400aab3b3302f858506316fe
Quiabera <rodrigo.gobatto@gmail.com>
2020-11-07 15:27:56 -0300

    WIP

--- src/components/inputName.components.tsx
+++ src/components/inputName.components.tsx
@@ -3,10 +3,8 @@ import "./component.styles.css";
 
 export function InputName(props: { placeholder: any; name: any, type: any, value: any, onChange: any}) {
 
-    const [inputName, setInputName] = useState("");
-
     function handleChange   (event: { target: { value: any;}; }){
-        setInputName(event.target.value);
+        props.onChange(event.target.value)
     }
    
     return(
@@ -14,11 +12,9 @@ export function InputName(props: { placeholder: any; name: any, type: any, value
             <input
             className="input"
             placeholder={props.placeholder}
-            name={props.name}
             type="text"
-            value={props.value ? props.value : inputName}
-            onChange={handleChange}
-            required />
+            value={props.value}
+            onChange={handleChange} />
             <br></br>
         </div>
             
--- src/page/newgame/new-game.page.tsx
+++ src/page/newgame/new-game.page.tsx
@@ -4,22 +4,22 @@ import "./new-game.page.styles.css";
 import { ButtonPage } from "../../components/buttonPage.component";
 import { InputName } from "../../components/inputName.components";
 import { equipName } from "../../scripts/equip-name";
-import { stringify } from "querystring";
 
 interface InputNameProps {
-  name: any
+  value: any
 }
 
 export function NewGame() {
 
-  const [inputName , setInputName] = useState<InputNameProps>({} as InputNameProps);
+  const [inputName , setInputName] = useState([]);
   
-  function handleChange(name: any) {
-    setInputName(name);
+  function handleChange(newValue: { preventDefault: () => void; }) {
+    newValue.preventDefault();
+    setInputName([...inputName]);
   }
   
   function handleClick() { 
-    console.log(inputName);
+    console.log(inputName[1]);
   }
 =======
 import * as React from 'react';
@@ -30,12 +30,12 @@ import * as React from 'react';
   return (
     <div className="novo">
       <h1>Equipe 1</h1>
-        <InputName placeholder="Nome 01" name="Nome01" type="text" value="" onChange={handleChange} />
-        <InputName placeholder="Nome 02" name="Nome02" type="text" value="" onChange={handleChange} />
+        <InputName placeholder="Nome 01" name="Nome01" type="text" value={inputName[0]} onChange={handleChange} />
+        <InputName placeholder="Nome 02" name="Nome02" type="text" value={inputName[1]} onChange={handleChange} />
       <br></br>
       <h1>Equipe 2</h1>
-        <InputName placeholder="Nome 03" name="Nome03" type="text" value="" onChange={handleChange} />
-        <InputName placeholder="Nome 04" name="Nome04" type="text" value="" onChange={handleChange} />
+        <InputName placeholder="Nome 03" name="Nome03" type="text" value={inputName[2]} onChange={handleChange} />
+        <InputName placeholder="Nome 04" name="Nome04" type="text" value={inputName[3]} onChange={handleChange} />
       <br></br>
       <button className="button" onClick={handleClick}>Combinar Nomes</button>
       <br></br>
