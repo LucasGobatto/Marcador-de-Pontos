0d414df22f9a48a854d4593686caa1b32a8101b7
LucasGobatto <lucas.bisaio@live.com>
2020-11-11 13:24:18 -0300

    little fix

---
+++ src/page/index.ts
@@ -0,0 +1,6 @@
+// simple way to simplify our imports from App.tsx
+export * from './continue';
+export * from './newgame';
+export * from './home';
+export * from './rules';
+export * from './initial';
