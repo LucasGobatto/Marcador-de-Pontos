6272f319d8754fad10f616f07eecc57f0d40fd35
Quiabera <rodrigo.gobatto@gmail.com>
2020-11-11 20:31:55 -0300

    Add: png files, addpoint and score page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import { Rules } from "./page/rules";
 import { About } from "./page/about"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ButtonURL } from "./components/buttonURL.component";
-
+import { Score } from './page/score';
+import { AddPoints } from './page/addpoints';
 
 export const App: React.FC = () => {
   return (
@@ -15,11 +16,13 @@ export const App: React.FC = () => {
       <div>
         <Switch>
           <Route path="/inicio" component={Home} />
-          <Route path="/novojogo" component={NewGame} />
+          <Route exact path="/novojogo" component={NewGame} />
           <Route path="/continue" component={Continue} />
           <Route path="/regras" component={Rules} />
           <Route path="/sobre" component={About} />
           <Route path="/regras/jogatina" component={ButtonURL} />
+          <Route exact path="/novojogo/placar" component={Score} />
+          <Route exact path="/novojogo/placar/adicionar" component={AddPoints} />
         </Switch>
       </div>
     </Router>
