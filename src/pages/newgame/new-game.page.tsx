3a59c083313bca95b183e8d9d82c403495704a20
LucasGobatto <lucas.bisaio@live.com>
2020-11-28 03:06:48 -0300

    create a flash message

--- src/pages/newgame/new-game.page.tsx
+++ src/pages/newgame/new-game.page.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './new-game.page.styles.css';
-import { ButtonPage, InputName, RandomNamesButton } from '@components';
+import { ButtonPage, FlashMessage, FlashMessageProps, InputName, RandomNamesButton } from '@components';
 import { equipName } from '@scripts/equip-name';
 
 interface PlayerNames {
@@ -14,6 +14,11 @@ export function NewGame() {
   const [inputName, setInputName] = useState<PlayerNames>(initialNames());
   const [randomName, setRandomName] = useState<string[]>([]);
   const [clicked, setClicked] = useState(false);
+  const [flashMessage, setFlashMessage] = useState<FlashMessageProps>({ visible: false });
+
+  React.useEffect(() => {
+    console.log(flashMessage);
+  }, [flashMessage]);
 
   function initialNames(): PlayerNames {
     return {
@@ -39,13 +44,21 @@ export function NewGame() {
       const teamName2 = equipName(inputName.player3.name, inputName.player4.name);
       setRandomName([teamName1, teamName2]);
     } else {
-      alert('Todos os campos devem ser preenchidos somente com letras');
+      setFlashMessage({
+        message: 'Todos os campos devem ser preenchidos somente com letras',
+        visible: true,
+        type: 'caption',
+      });
     }
   }
 
   function handleClick() {
     setClicked(true);
     handleValidation();
+  }
+
+  function closeFlashMessage() {
+    setFlashMessage({ visible: false });
   }
 
   return (
@@ -84,6 +97,12 @@ export function NewGame() {
         <RandomNamesButton title="Combinar Nomes" onClick={handleClick} />
         <ButtonPage title="Inicio" icon="home" />
       </div>
+      <FlashMessage
+        onClick={closeFlashMessage}
+        visible={flashMessage.visible}
+        type={flashMessage.type}
+        message={flashMessage.message}
+      />
     </div>
   );
 }
